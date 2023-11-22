b942be28c63b79de16a2bcbc7f1305723a8cd5a2
Ethan Berkebile <ethanberkebile@gmail.com>
2023-11-22 16:14:57 -0600

    Added .catch/next and use of middleware

--- server/server.js
+++ server/server.js
@@ -1,88 +1,66 @@
-import express from "express";
-import pg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config({ path: "../.env" });
-
-const { PORT, DATABASE_URL } = process.env;
-
-const client = new pg.Client({
-  connectionString: DATABASE_URL,
-});
-
-await client.connect();
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Steam Page");
-});
-
-app.get("/api/videogames", (req, res) => {
-  client.query("SELECT * FROM videogames").then((rows) => {
-    res.send(rows);
-  });
-});
-
-app.post("/api/videogames", (req, res) => {
+app.post("/api/videogames", (req, res, next) => {
   const { name, age_rating, genre, rating } = req.body;
   client.query(
     "INSERT INTO videogames(name, age_rating, genre, rating) VALUES($1, $2, $3, $4) RETURNING *",
     [name, age_rating, genre, rating]
-    )
-    .then((data) => {
-      res.status(201).res.json(data.rows[0]);
-    })
-    .catch((error) => {
-      console.error("Error creating videogame: ", error);
-      res.status(500).json({ error: "Internal server error" });
-    });
+  )
+  .then((data) => {
+    res.status(201).json(data.rows[0]);
+  })
+  .catch((error) => {
+    next(error);
+  });
 });
 
-app.patch("/api/videogames/:id", (req, res) => {
+app.patch("/api/videogames/:id", (req, res, next) => {
   const videogameId = Number.parseInt(req.params.id);
   const { name, age_rating, genre, rating } = req.body;
-client
+  client
     .query(
       `UPDATE videogames SET
       name = COALESCE ($1, name),
       age_rating = COALESCE ($2, age_rating), 
       genre = COALESCE ($3, genre),
-      rating = COALESCE ($4, rating), 
+      rating = COALESCE ($4, rating) 
       WHERE id=$5 RETURNING *`,
-    [name, age_rating , genre, rating, videogameId]
+      [name, age_rating, genre, rating, videogameId]
     )
-      .then((data) => {
-        if (data.rows.length == 0){
-          res.sendStatus(404);
-        }
-        console.log("updated videogame: ", data.rows[0]);
+    .then((data) => {
+      if (data.rows.length === 0) {
+        res.sendStatus(404);
+      } else {
+        console.log("Updated videogame: ", data.rows[0]);
         res.send(data.rows[0]);
-      }).catch((err) => {
-        console.error(err);
-        res.sendStatus(500);
-      })
+      }
     })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-app.delete("/api/videogames/:id", (req, res) => {
+app.delete("/api/videogames/:id", (req, res, next) => {
   const id = Number(req.params.id);
-  client.query(
-    "DELETE FROM videogames WHERE id = $1 RETURNING *",
-    [id])
+  client
+    .query("DELETE FROM videogames WHERE id = $1 RETURNING *", [id])
     .then((data) => {
       if (data.rows.length === 0) {
         res.sendStatus(404);
       } else {
         res.send(data.rows[0]);
       }
-    }).catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
     })
-})
+    .catch((err) => {
+      next(err);
+    });
+});
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+// Additional error handling for unknown routes
+app.use((req, res, next) => {
+  res.status(404).send("Not Found");
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error("Global error: ", err);
+  res.status(500).json({ error: "Internal Server Error" });
 });
