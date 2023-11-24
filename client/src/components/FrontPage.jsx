73bdd84728e61bbec2b6f419990d9f29f395371f
Greg Pappas <gnpappas@proton.me>
2023-11-24 11:37:00 -1000

    4x3 grid with games images and title

---
+++ client/src/components/FrontPage.jsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const FrontPage = () => {
+	const [games, setGames] = useState([]);
+
+	const getGames = async () => {
+		try {
+			const response = await axios.get("/api/games");
+			setGames(response.data.games);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		getGames();
+	}, []);
+
+	return (
+		<>
+			<div className="mx-auto grid grid-cols-3 px-2">
+				{games.length !== 0 ? (
+					games.map((game) => {
+						return (
+							<div className="pb-2">
+								<img
+									src={game.background_image}
+									alt="games"
+									className="h-60 w-[100%]"
+								/>
+								<p className="text-white font-bold">{game.name}</p>
+							</div>
+						);
+					})
+				) : (
+					<p className="uppercase text-2xl text-white font-bold">
+						Loading games...
+					</p>
+				)}
+			</div>
+		</>
+	);
+};
+
+export default FrontPage;
