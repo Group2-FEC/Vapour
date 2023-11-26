77efd6596d749129ebd9306308627172ecc5208d
Greg Pappas <gnpappas@proton.me>
2023-11-26 12:10:00 -1000

    Add Library component to App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./Header";
 import FrontPage from "./FrontPage";
 import Footer from "./Footer";
+import Library from "./Library";
 // import ModelReference from "./ModelReference";
 const App = () => {
 	return (
 		<div className="min-h-[100dvh]">
 			<Header />
-			<FrontPage />
+			{/* <FrontPage/> */}
+			<Library />
 			<Footer />
 		</div>
 	);
