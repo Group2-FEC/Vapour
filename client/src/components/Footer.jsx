a6f235c4a03219176d5fc9e1bc80d66c528f3ca9
Greg Pappas <gnpappas@proton.me>
2023-11-25 14:42:12 -1000

    Fix footer bg color when in light mode

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 shadow flex items-center p-2 bg-gray-800 border-gray-600">
+		<footer className="fixed bottom-0 left-0 w-full border-t border-gray-200 shadow flex items-center p-2 bg-gray-800 border-gray-600">
 			<div className="text-sm text-blue-200 mx-auto font-bold">
 				{new Date().getFullYear().toString()} | Team Steam
 			</div>
