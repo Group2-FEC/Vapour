20c8d26d9ef1f1a4df39aa8524d5c37a290cdeec
Greg Pappas <gnpappas@proton.me>
2023-11-27 13:07:54 -1000

    Handle merge conflicts

--- client/src/components/FrontPage.jsx
+++ client/src/components/FrontPage.jsx
@@ -4,11 +4,23 @@ import { useEffect, useState } from "react";
 
 const FrontPage = () => {
 	const [games, setGames] = useState([]);
+	const [gameInfo, setGameInfo] = useState({});
+	const [showInfo, setShowInfo] = useState(false);
 
-	const getGames = async () => {
+  const getGames = async () => {
+    try {
+      const response = await axios.get("/api/games");
+      setGames(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+	const getGameDetails = async (id) => {
 		try {
-			const response = await axios.get("/api/games");
-			setGames(response.data);
+			const response = await axios.get(`/api/game/${id}`);
+			setGameInfo(response.data);
+			setShowInfo(true);
 		} catch (error) {
 			console.error(error);
 		}
@@ -17,20 +29,26 @@ const FrontPage = () => {
 	useEffect(() => {
 		getGames();
 	}, []);
+
+	const handleCloseButton = () => {
+		setGameInfo(false);
+	};
+
 	return (
-		<>
-			<div className="w-5/6 mx-auto grid grid-cols-3 gap-2 rounded-b bg-gradient-to-r from-blue-200/40 to-blue-500/40 mb-10 p-2">
+		<div className="relative" onClick={() => setShowInfo(false)}>
+			<div className="w-5/6 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 rounded-b bg-gradient-to-r from-blue-200/40 to-blue-500/40 mb-10 p-2">
 				{games.length !== 0 ? (
 					games.map((game) => {
 						return (
 							<div
 								key={game.id}
-								className="shadow-xl"
+								className="shadow-xl cursor-pointer"
+								onClick={() => getGameDetails(game.id)}
 							>
 								<img
 									src={game.background_image}
 									alt="games"
-									className="h-48 w-[100%] rounded-t"
+									className="h-48 w-full rounded-t"
 								/>
 								<p className="text-center text-white text-xl font-bold bg-blue-400/30 rounded-b border-b-4 border-blue-300/60">
 									{game.name}
@@ -44,7 +62,10 @@ const FrontPage = () => {
 					</p>
 				)}
 			</div>
-		</>
+			{showInfo && (
+				<GameInfo gameInfo={gameInfo} handleCloseButton={handleCloseButton} />
+			)}
+		</div>
 	);
 };
 
