d7260fd63edfc8e4e4ee585d8d7f5bb9d74031b4
Ethan Berkebile <ethanberkebile@gmail.com>
2023-11-27 11:30:59 -0600

    Input to handle search queries

--- client/src/components/Wishlist.jsx
+++ client/src/components/Wishlist.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 const Wishlist = () => {
     const [wishlist, setWishlist] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+
 
     const getWishlist = async () => {
 		try {
@@ -13,33 +15,46 @@ const Wishlist = () => {
 		}
 	};
 
+    const handleSearch = async () => {
+        try {
+          const response = await axios.get(`api/games?search=${searchQuery}`);
+          const gameData = response.data;
+
+          if (gameData.length > 0) {
+            const firstGame = gameData[0];
+            await axios.post('api/videogames', firstGame);
+          } else {
+            console.log('No games found.');
+          }
+        } catch (error) {
+          console.error(error);
+        }
+    };
+
 	//onClick render wishlist and remove library component
-	return (
-		<div className="w-5/6 mx-auto flex gap-2 rounded-b bg-gradient-to-r from-blue-200/40 to-blue-500/40 mb-10 p-2 mb-10">
-        <div id="wishlist" className="w-1/6 flex  flex-col gap-2">
-        <input type="text" name="search" id="search" placeholder="search" className="p-1"/>
-        {wishlist.length !== 0 ? (
-            wishlist.map((game) => (
-                <p className="text-slate-200 font-bold text-sm">{game.name}</p>
-            ))
-        ) : (
-            <p className="text-white font-bold">Loading Wishlist</p>
-        )}
-    </div>
-			<div
-				id="gallery"
-				className="w-5/6 grid grid-cols-5 border-l border-white/40 gap-2 pl-2"
-			>
-				{wishlist.length !== 0 ? (
-					wishlist.map((game) => (
-						<img src={game.background_image} alt="games" className="h-full rounded shadow-xl border border-white/40" />
-					))
-				) : (
-					<p className="text-white font-bold">Loading images</p>
-				)}
-			</div>
-		</div>
-	);
-};
+    return (
+        <div className="w-5/6 mx-auto flex gap-2 rounded-b bg-gradient-to-r from-blue-200/40 to-blue-500/40 mb-10 p-2 mb-10">
+          <div id="wishlist" className="w-1/6 flex flex-col gap-2">
+            <input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Add to Wishlist"
+              className="p-1"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  handleSearch();
+                }
+              }}
+            />
+            {/* Render wishlist items */}
+          </div>
+          {/* Render gallery */}
+        </div>
+      );
+    };
+  
 
 export default Wishlist;
