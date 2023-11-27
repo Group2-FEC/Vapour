ea45304764b7902db19a096808e97d6cecfb1002
Ethan Berkebile <ethanberkebile@gmail.com>
2023-11-27 12:33:52 -0600

    Updated Database table to pull specific api data

--- server/migration.sql
+++ server/migration.sql
@@ -3,9 +3,10 @@ DROP TABLE IF EXISTS videogames;
 CREATE TABLE videogames (
   id SERIAL PRIMARY KEY,
   name TEXT,
-  age_rating TEXT,
-  genre TEXT,
+  background_image TEXT,
+  esrb_rating TEXT,
+  released TEXT,
   rating INT
 );
 
-INSERT INTO videogames(name, age_rating, genre, rating) VALUES('Halo', 'M', 'FPS', '92');
+INSERT INTO videogames(name, background_image, esrb_rating, released, rating) VALUES('Halo', 'https://media.rawg.io/media/screenshots/aac/aaccfdc54ef56523e18337a3cfae7d7f.jpg', 'Mature', '2001-11-15', '4');
