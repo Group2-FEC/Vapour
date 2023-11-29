eb9d4f15454a210f1a5066d5f3750d55262c5425
Greg Pappas <gnpappas@proton.me>
2023-11-28 14:48:42 -1000

    Add routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import { Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import FrontPage from "./FrontPage";
 import GameInfo from "./GameInfo";
@@ -8,39 +9,48 @@ import Wishlist from "./Wishlist";
 import Footer from "./Footer";
 import Upcoming from "./Upcoming";
 import GameCarousel from "./GameCarousel";
-// import ModelReference from "./ModelReference";
 const App = () => {
-  const [gameInfo, setGameInfo] = useState({});
-  const [showInfo, setShowInfo] = useState(false);
+	const [gameInfo, setGameInfo] = useState({});
+	const [showInfo, setShowInfo] = useState(false);
 
-  const getGameDetails = async (id) => {
-    try {
-      const response = await axios.get(`/api/game/${id}`);
-      setGameInfo(response.data);
-      setShowInfo(!showInfo);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+	const getGameDetails = async (id) => {
+		try {
+			const response = await axios.get(`/api/game/${id}`);
+			setGameInfo(response.data);
+			setShowInfo(!showInfo);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  const handleCloseButton = () => {
-    setShowInfo(!showInfo);
-  };
+	const handleCloseButton = () => {
+		setShowInfo(!showInfo);
+	};
 
-  return (
-    <div className="min-h-[100dvh]">
-      <Header />
-      <Upcoming getGameDetails={getGameDetails} />
-      {/* <Wishlist/> */}
-      {/* <FrontPage getGameDetails={getGameDetails} /> */}
-      {/* <Library getGameDetails={getGameDetails} /> */}
-      {showInfo && (
-        <GameInfo gameInfo={gameInfo} handleCloseButton={handleCloseButton} />
-      )}
-      <GameCarousel />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="min-h-[100dvh]">
+			<Header />
+			<Routes>
+				<Route
+					path="/"
+					element={<FrontPage getGameDetails={getGameDetails} />}
+				></Route>
+				<Route
+					path="/upcoming"
+					element={<Upcoming getGameDetails={getGameDetails} />}
+				></Route>
+				<Route
+					path="/library"
+					element={<Library getGameDetails={getGameDetails} />}
+				></Route>
+				<Route path="/wishlist" element={<Wishlist />}></Route>
+			</Routes>
+			{showInfo && (
+				<GameInfo gameInfo={gameInfo} handleCloseButton={handleCloseButton} />
+			)}
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
