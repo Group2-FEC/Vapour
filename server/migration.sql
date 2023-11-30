746ed6502fb9481799a05c7fc309f2321f3b0cd1
Ethan Berkebile <ethanberkebile@gmail.com>
2023-11-30 11:05:07 -0600

    Added Unique Constraint to videogame name value

--- server/migration.sql
+++ server/migration.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS videogames;
 
 CREATE TABLE videogames (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
+  name TEXT NOT NULL UNIQUE,
   background_image TEXT NOT NULL,
   esrb_rating TEXT,
   released TEXT NOT NULL,
@@ -10,4 +10,4 @@ CREATE TABLE videogames (
 );
 
 INSERT INTO videogames(name, background_image, esrb_rating, released, rating) VALUES('Halo', 'https://media.rawg.io/media/screenshots/aac/aaccfdc54ef56523e18337a3cfae7d7f.jpg', 'Mature', '2001-11-15', '4');
-INSERT INTO videogames(name, background_image, esrb_rating, released, rating) VALUES('Halo2', 'https://media.rawg.io/media/games/3bf/3bfc3bd9fda76bf83f6cf1d788e1c7c7.jpg', 'Mature', '2004-11-09', '4.38');
+INSERT INTO videogames(name, background_image, esrb_rating, released, rating) VALUES('Halo 2', 'https://media.rawg.io/media/games/3bf/3bfc3bd9fda76bf83f6cf1d788e1c7c7.jpg', 'Mature', '2004-11-09', '4.38');
