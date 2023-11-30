dc48de6860abc45abc9f79e8daca5e0eef5be052
Greg Pappas <gnpappas@proton.me>
2023-11-30 07:28:01 -1000

    Add logo and user bar. Adjust responsive design

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,46 +1,47 @@
-import notification from "../img/icons/notification.png";
-import announcement from "../img/icons/announcement.png";
-import userImage from "../img/icons/user.png";
+import notification from "../assets/icons/notification.png";
+import announcement from "../assets/icons/announcement.png";
+import userImage from "../assets/icons/user.png";
+import logo from "../assets/icons/logo.svg";
 import Navbar from "./Navbar";
 
 const Header = ({ wishlist }) => {
-	return (
-		<div className="bg-slate-700/70 border-b border-gray-600">
-			<header>
-				<div
-					id="userBar"
-					className="flex justify-end items-center px-5 pt-1 gap-2 py-3"
-				>
-					<img
-						className="w-10 bg-sky-200 p-2 rounded"
-						src={announcement}
-						alt="announcement"
-					/>
-					<img
-						className="w-10 bg-green-200 p-2 rounded"
-						src={notification}
-						alt="notification"
-					/>
-					<div className="flex items-center gap-1">
-						<img
-							className="w-10 bg-red-200 p-2 rounded"
-							src={userImage}
-							alt="user"
-						/>
-					</div>
-				</div>
-
-				<div
-					id="storeLinks"
-					className="w-5/6 mx-auto flex md:justify-between justify-center items-center bg-gradient-to-r from-blue-400 to-blue-800 rounded-t p-2"
-				>
-					<div className="flex gap-2 text-white font-bold sm:gap-5">
-						<Navbar wishlist={wishlist} />
-					</div>
-				</div>
-			</header>
-		</div>
-	);
+  return (
+    <header className="relative bg-slate-700/70 border-b border-gray-600">
+      <img
+        src={logo}
+        alt="logo"
+        className="w-80 mx-auto drop-shadow"
+        style={{ marginBottom: "-18px" }}
+      />
+      <div
+        id="storeLinks"
+        className="w-5/6 mx-auto flex sm:justify-between sm:flex-row flex-col sm:items-center items-end justify-center bg-gradient-to-r from-blue-400 to-blue-800 rounded-t p-2"
+      >
+        <div className="flex gap-2 text-white font-bold sm:gap-5 sm:order-first order-last">
+          <Navbar wishlist={wishlist} />
+        </div>
+        <div id="userBar" className="flex gap-2">
+          <img
+            className="sm:w-10 w-8 bg-sky-200 p-2 rounded shadow-lg"
+            src={announcement}
+            alt="announcement"
+          />
+          <img
+            className="sm:w-10 w-8 bg-green-200 p-2 rounded shadow-lg"
+            src={notification}
+            alt="notification"
+          />
+          <div className="flex items-center gap-1">
+            <img
+              className="sm:w-10 w-8 bg-red-200 p-2 rounded shadow-lg"
+              src={userImage}
+              alt="user"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
