cab3738ce8a70afafc8d138127ce8309af5b6589
Mark Northup <marknorthup@Marks-Air.attlocal.net>
2023-12-01 10:10:43 -0600

    underlined in the navbar for the component you are in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,35 @@
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
 const Navbar = ({ wishlist }) => {
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState("");
+
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location.pathname]);
+
   return (
     <>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/library"}>Library</Link>
-      <Link to={"/upcoming"}>Upcoming</Link>
-      <Link to={"/wishlist"}>
+      <Link to={"/"} className={activeLink === "/" ? "underline" : ""}>
+        Home
+      </Link>
+      <Link
+        to={"/library"}
+        className={activeLink === "/library" ? "underline" : ""}
+      >
+        Library
+      </Link>
+      <Link
+        to={"/upcoming"}
+        className={location.pathname === "/upcoming" ? "underline" : ""}
+      >
+        Upcoming
+      </Link>
+      <Link
+        to={"/wishlist"}
+        className={location.pathname === "/wishlist" ? "underline" : ""}
+      >
         {wishlist.length !== 0 ? `Wishlist (${wishlist.length})` : `Wishlist`}
       </Link>
     </>
