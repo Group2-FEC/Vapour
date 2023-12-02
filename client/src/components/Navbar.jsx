9088f896ef2ff3d85aa99ce30a4d5f6436372aa2
Greg Pappas <gnpappas@proton.me>
2023-12-01 20:36:41 -1000

    Convert wishlist prop to useContext

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { WishContext } from "./App";
 
-const Navbar = ({ wishlist }) => {
+const Navbar = () => {
   const location = useLocation();
   const [activeLink, setActiveLink] = useState("");
+  const wishlist = useContext(WishContext);
 
   useEffect(() => {
     setActiveLink(location.pathname);
@@ -11,24 +13,24 @@ const Navbar = ({ wishlist }) => {
 
   return (
     <>
-      <Link to={"/"} className={activeLink === "/" && "underline"}>
+      <Link to={"/"} className={activeLink === "/" ? "underline" : undefined}>
         Home
       </Link>
       <Link
         to={"/library"}
-        className={activeLink === "/library" && "underline"}
+        className={activeLink === "/library" ? "underline" : undefined}
       >
         Library
       </Link>
       <Link
         to={"/upcoming"}
-        className={activeLink === "/upcoming" && "underline"}
+        className={activeLink === "/upcoming" ? "underline" : undefined}
       >
         Upcoming
       </Link>
       <Link
         to={"/wishlist"}
-        className={activeLink === "/wishlist" && "underline"}
+        className={activeLink === "/wishlist" ? "underline" : undefined}
       >
         {wishlist.length !== 0 ? `Wishlist (${wishlist.length})` : `Wishlist`}
       </Link>
