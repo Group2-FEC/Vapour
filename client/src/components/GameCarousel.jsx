7f8ab768e5dba284c8b0a09833e8d8fdce431582
Greg Pappas <gnpappas@proton.me>
2023-12-03 16:23:42 -1000

    Add close button and make text responsive

--- client/src/components/GameCarousel.jsx
+++ client/src/components/GameCarousel.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import GameCaseComp from "./GameCaseComp";
 import { useSpring, animated } from "react-spring";
 
-const GameCarousel = () => {
+const GameCarousel = ({ handleShowSale }) => {
   const [time, setTime] = useState(13 * 60 + 15); // 13 hours and 15 minutes in minutes
 
   useEffect(() => {
@@ -29,21 +29,22 @@ const GameCarousel = () => {
     return () => clearInterval(interval);
   }, [color]);
 
-  // Calculate the gradient colors
-  const color1 = `hsl(${(color + 0) % 360}, 0%, 0%)`; // Black
-  const color2 = `hsl(${(color + 0) % 360}, 0%, 30%)`; // Light Gray
-  const color3 = `hsl(${(color + 0) % 360}, 0%, 0%)`; // Black
-
   return (
     <>
-      <div
-        className="relative flex flex-row m-auto mt-10 mb-10"
-        style={{
-          background: `linear-gradient(45deg, ${color1}, ${color2}, ${color3})`,
-          backgroundSize: "600% 600%",
-          animation: "Gradient 6s ease infinite",
-        }}
-      >
+      <div className="relative flex flex-row mx-auto w-5/6 bg-slate-800 p-2">
+        <div className="z-10 absolute right-0 top-0 cursor-pointer">
+          <svg
+            width="50px"
+            height="50px"
+            viewBox="0 0 24 24"
+            onClick={handleShowSale}
+          >
+            <path
+              d="M8.00191 9.41621C7.61138 9.02569 7.61138 8.39252 8.00191 8.002C8.39243 7.61147 9.0256 7.61147 9.41612 8.002L12.0057 10.5916L14.5896 8.00771C14.9801 7.61719 15.6133 7.61719 16.0038 8.00771C16.3943 8.39824 16.3943 9.0314 16.0038 9.42193L13.4199 12.0058L16.0039 14.5897C16.3944 14.9803 16.3944 15.6134 16.0039 16.004C15.6133 16.3945 14.9802 16.3945 14.5896 16.004L12.0057 13.42L9.42192 16.0038C9.03139 16.3943 8.39823 16.3943 8.00771 16.0038C7.61718 15.6133 7.61718 14.9801 8.00771 14.5896L10.5915 12.0058L8.00191 9.41621Z"
+              fill="#ffffff"
+            />
+          </svg>
+        </div>
         {/* Place your 3D models here */}
         <GameCaseComp
           cover="src/assets/baldurs_gate.jpg"
@@ -78,7 +79,7 @@ const GameCarousel = () => {
                 (t) => `scale(${t}) translateX(-50%)`
               ),
             }}
-            className=" bg-lime-500 font-extrabold text-white bg-opacity-80 rounded-md text-center text-7xl p-2 m-1 shadow-lg pointer-events-auto animate-pulse"
+            className=" bg-lime-500 font-extrabold text-white bg-opacity-80 rounded-md text-center sm:text-6xl text-4xl p-2 m-1 shadow-lg pointer-events-auto animate-pulse"
           >
             Holiday Sale
           </animated.div>
@@ -89,7 +90,7 @@ const GameCarousel = () => {
                 (t) => `scale(${t}) translateX(50%)`
               ),
             }}
-            className=" bg-red-500 font-bold text-xl text-white bg-opacity-80 rounded-md text-center p-2 m-1 shadow-lg pointer-events-auto animate-pulse"
+            className=" bg-red-500 font-bold sm:text-xl text-white bg-opacity-80 rounded-md text-center p-2 m-1 shadow-lg pointer-events-auto animate-pulse"
           >
             Only {String(Math.floor((time % 3600) / 60)).padStart(2, "0")} hrs
             and {String(Math.floor(time % 60)).padStart(2, "0")} mins left!
@@ -99,7 +100,7 @@ const GameCarousel = () => {
               ...fade,
               backgroundColor: "red",
             }}
-            className="text-white text-2xl font-bold shadow-md rounded-md text-center p-2 m-1 mt-5 transition duration-300 ease-in-out pointer-events-auto animate-bounce"
+            className="text-white sm:text-2xl text-lg font-bold shadow-md rounded-md text-center p-2 m-1 mt-5 transition duration-300 ease-in-out pointer-events-auto animate-bounce"
             onClick={() =>
               window.open("https://github.com/Group2-FEC/Vapour", "_blank")
             }
