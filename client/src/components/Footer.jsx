34c305d76ee04cd9ee55fa7f54228ed3fde0e0cd
Greg Pappas <gnpappas@proton.me>
2023-12-04 20:33:33 -1000

    Unfix footer; set to standard display

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full border-t border-gray-200 shadow flex items-center p-2 bg-gray-800 border-gray-600">
+    <footer className="text-center p-2 bg-blue-600/20 border-b border-blue-700/40">
       <div className="text-sm text-blue-200 mx-auto font-bold">
         {new Date().getFullYear().toString()} | Team Steam | Careers | Support |
         Contact
